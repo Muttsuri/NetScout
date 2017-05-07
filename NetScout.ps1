c616725a1d3783e2e9049dbd43733a8745394289
Muttsuri <Muttsuri@outlook.com>
2017-05-07 01:13:15 +0100

    Recuperação de Espaço, Organização e Caracters Especiais
    Resolução do problema de apresentar o espaço limpo após a limpeza do
    script.
    Resolvido com o uso do array $recArray (apesar de causar a apresentação
    do indice do array cada vez que $free é salvo para o array)
    
    Organização do output.
    Remoção do "foreach" desnecessario a $colItems
    Organizaçao do output por order crescente de especificação:
    *Fabricante
    *Modelo
    *CPU
    *RAM
    *OS
    *Username
    
    Caracters especias a não apresentarm corretamente resolvido por salvar o
    script com Encoding: "UTF-8 with BOM"

--- NetScout.ps1
+++ NetScout.ps1
@@ -1,25 +1,27 @@
-<#   
+﻿<#   
 .SYNOPSIS   
 <Estre script foi criado para encontrar o IPV4, Marca, Modelo , RAM, CPU, Numero de Serie, Utilizador Corrente e Estatísticas de Disco, de uma maquina através do seu nome. 
-Também limpará o disco de ficheiros de lixo caso o utilizador o considere necessário>  
+Também limpará o disco de ficheiros de lixo caso o utilizador o considere necessário e apresentará o espaço recuperado.>  
 .DESCRIPTION 
 <O Script através do nome da maquina irá pingar a maquina e depois irá recolher a informação da maquina e depois apresenta-a. A informação que este Script dará é:  
 O nome do utilizador ligado na altura da verificação.  
 O Nome do Fabricante.  
 O Modelo da Maquina.  
-O IPV4  
-O Nome do CPU  
-O Nome do Sistema Operativo  
-O Numero de Serie  
-O Utilizador Currente  
-O Tamanho dos discos C e D  
-O Espaço Ocupado dos discos C e D  
-O Espaço Livre dos discos C e D 
-Após isso ele dará ao utilizador a escolha de correr uma função de limpeza baseada num script por Nuno Almeida>  
-.PARAMETER <Nome do PC>  
-.INPUTS  
+O IPV4.
+O Nome do CPU.
+O Nome do Sistema Operativo.
+O Numero de Serie.
+O Utilizador Currente.
+O Tamanho dos discos C e D.
+O Espaço Ocupado dos discos C e D.
+O Espaço Livre dos discos C e D.
+Após isso ele dará ao utilizador a escolha de correr uma função de limpeza baseada num script por Nuno Almeida.
+E apresentará o espaço Recupeado pelo script de limpeza.>  
+.PARAMATER
+<Nome do PC>  
+.INPUTS 
 <Nome do pc que será perguntado pelo script. Este script não tem argumentos>  
-.OUTPUTS  
+.OUTPUTS
 <Output aparece na consola:   
    IPv4  
    Numero de Serie  
@@ -30,90 +32,100 @@ Após isso ele dará ao utilizador a escolha de correr uma função de limpeza b
    Utilizador currente  
    RAM  
    Tamanho de C e D  
-   Espaço Ocupado e Livre de C e D>  
-.NOTES  
-Version: 3  
+   Espaço Ocupado e Livre de C e D
+   Espaço Recuperado após a limpeza caso esta seja escolhida>  
+.NOTES 
+Version: 4  
 Author: <João Aelxandre Garica Correia>  
-Creation Date: <15/04/2017>  
+Creation Date: <07/05/2017>  
 Purpose/Change: Network Scouting and Remote Disk Cleaning 
 #>  
- 
+
 function RDiskCleanCmd #disk cleaning script
-{ cmd.exe /c "C:\Windows\System32\CleanPC.cmd" 
+{ cmd.exe /c "C:\Windows\System32\CleanPC.cmd"
 #second scan of disks and compatrison with the first
 $diskscan = Get-WmiObject Win32_logicaldisk -ComputerName $pcname
-foreach ($dskobj in $diskscan) {  
-if ($dskobj.VolumeName -eq $null ) {}  
-else {Write-Host $dskobj.deviceid 
-$max2 = [math]::round($dskobj.Size/1024/1024/1024, 0)        
-$free2 = [math]::round($dskobj.FreeSpace/1024/1024/1024, 0)         
-$full2 = $max2 - $free2    
-Write-Host "Espaço Total em Disco"$dskobj.deviceid":" $max2 "GB"   
-Write-Host "Espaço Ocupado em Disco"$dskobj.deviceid":" $full2 "GB"   
-Write-Host "Espaço Livre em Disco"$dskobj.deviceid":" $free2 "GB"   
-Write-Host
-foreach ($diskitem in $diskspace) {
-$dif = $free2 - $free 
-Write-Host $dskitem.deviceid
-Write-Host "Recovered Space"$dif "GB"}
- }} #end of disk info
-}#end disk cleaning script
+$cnt = 0
+foreach ($dskobj in $diskscan) {
+if ($dskobj.VolumeName -eq $null ) {}
+else {Write-Host $dskobj.deviceid
+$max2 = [math]::round($dskobj.Size/1024/1024/1024, 0)
+$free2 = [math]::round($dskobj.FreeSpace/1024/1024/1024, 0)
+$full2 = $max2 - $free2
+$dif = $free2 - $recArray[$cnt]
+Write-Host "Espaço Total em Disco"$dskobj.deviceid":" $max2 "GB"
+Write-Host "Espaço Ocupado em Disco"$dskobj.deviceid":" $full2 "GB"
+Write-Host "Espaço Livre em Disco"$dskobj.deviceid":" $free2 "GB"
+Write-Host "Recovered Space"$dif "GB"
+$cnt++
+catch [System.Management.Automation.ItemNotFoundException] {} }
+} 
+ } #end of disk info
+#end disk cleaning script
 
-function main { #core of the script
+#Codigo central ao script
 While (!$pcname){      
-$pcname = Read-Host -Prompt "Insira o nome do pc para obter informaão"}   
+$pcname = Read-Host -Prompt "Insira o nome do pc para obter informção"}   
    
-$contest = Test-Connection -ComputerName $pcname -count 1 -Quiet #ver se está ligado 
+$contest = Test-Connection -ComputerName $pcname -count 1 -Quiet #Teste para saber se o pc está ligado
 if ($contest -eq 1 ) {   
-$iv4 = Test-Connection -ComputerName $pcname -count 1 -ErrorAction Stop| select-object IPV4Address # IPv4  
+$iv4 = Test-Connection -ComputerName $pcname -count 1 -ErrorAction Stop| select-object IPV4Address #Recolha do IPv4 de $pcname
 Write-Host "IPv4: "$iv4.Ipv4address   
 Write-Host   } 
+
 if ($contest -eq 1 ) {       
-Try { 
-$colItems = Get-wmiobject -ErrorAction Stop -class Win32_ComputerSystem -namespace "root\CIMV2" -computername $pcname     
-$osname = Get-WmiObject -ComputerName $pcname -Class Win32_OperatingSystem | Select-Object caption  #sistema operativo 
-$cpuname = Get-WmiObject Win32_processor -ComputerName $pcname | Select-Object name #CPU   
+Try {
+#Recolha de informação geral
+$colItems = Get-wmiobject -ErrorAction Stop -class Win32_ComputerSystem -namespace "root\CIMV2" -computername $pcname #Recolha de uma variadade de informação e deteção de erros com terminação de script caso exeções sejam encontradas
+$osname = Get-WmiObject -ComputerName $pcname -Class Win32_OperatingSystem | Select-Object caption  #Nome do Sistema Operativo 
+$cpuname = Get-WmiObject Win32_processor -ComputerName $pcname | Select-Object name #Nome CPU   
 $serial = Get-WmiObject win32_bios -ComputerName $pcname | Select-Object SerialNumber  #numero de serie 
-Write-Host "Numero de Serie:" $serial.SerialNumber   
-Write-Host "CPU: " $cpuname.name    
-Write-Host "OS: " $osname.caption  
-   
-foreach ($objItem in $colItems){  #PC iinfo   
-Write-Host "Fabricante: " $objItem.Manufacturer #Nome de Fabricante     
-write-host "Modelo: " $objItem.Model #Nome de Modelo     
-Write-Host "Username: " $objItem.UserName #username     
-$displayGB = [math]::round($objItem.TotalPhysicalMemory/1024/1024/1024, 0) #Formatação do output da quantidade de RAM para GB     
-write-host "RAM: " $displayGB "GB" } #output da quantidade de RAM em GB  
-    
-$diskspace = Get-WmiObject Win32_logicaldisk -ComputerName $pcname #disk info
-foreach ($dskitem in $diskspace) {  
-if ($dskitem.VolumeName -eq $null ) {}  
-else {Write-Host $dskitem.deviceid 
-$max = [math]::round($dskitem.Size/1024/1024/1024, 0)        
-$free = [math]::round($dskitem.FreeSpace/1024/1024/1024, 0)         
+$displayGB = [math]::round($colItems.TotalPhysicalMemory/1024/1024/1024, 0) #Formatação de RAM para GB
+#Inicio de display de resultados gerais 
+Write-Host "Fabricante: " $colItems.Manufacturer #Nome de Fabricante     
+write-host "Modelo: " $colItems.Model #Nome de Modelo 
+Write-Host "Numero de Serie:" $serial.SerialNumber    
+Write-Host "CPU: " $cpuname.name  #Nome do CPU output
+write-host "RAM: " $displayGB "GB"  #RAM em GB output
+Write-Host "OS: " $osname.caption #Sistema Operativo output
+Write-Host "Username: " $colItems.UserName #Nome de utilizador   
+#fim do display de resultados gerais
+Write-Host
+Write-Host "Procurando discos..." #A recolha de informação de discos pode demorar
+Write-Host
+$diskspace = Get-WmiObject Win32_logicaldisk -ComputerName $pcname #Recolha de informação de disco
+$recArray = [System.Collections.ArrayList]@() #array para onde salvar $free para poder ser comparado mais tarde
+#Inicio display de dados de disco
+foreach ($dskitem in $diskspace) {
+if ($dskitem.VolumeName -eq $null ) {}
+else {Write-Host $dskitem.deviceid
+$max = [math]::round($dskitem.Size/1024/1024/1024, 0)      
+$free = [math]::round($dskitem.FreeSpace/1024/1024/1024, 0)
+$recArray.Add($free)
 $full = $max - $free    
-Write-Host "Espaço Total em Disco"$dskitem.deviceid":" $max "GB"   
-Write-Host "Espaço Ocupado em Disco"$dskitem.deviceid":" $full "GB"   
-Write-Host "Espaço Livre em Disco"$dskitem.deviceid":" $free "GB"   
-Write-Host }} #end of disk info
-#$yn = Read-Host -Prompt "Deseja Fazer a limpeza remota de disco (S/N or Y/N)"  #cleaning pompt
+Write-Host "Espaço Total em Disco"$dskitem.deviceid":" $max "GB"
+Write-Host "Espaço Ocupado em Disco"$dskitem.deviceid":" $full "GB"
+Write-Host "Espaço Livre em Disco"$dskitem.deviceid":" $free "GB"
+Write-Host }} 
+
+$yn = Read-Host -Prompt "Deseja Fazer a limpeza remota de disco (S/N or Y/N)"  #Prompt de limpeza
 if ($yn -ne $null -Or "n") {
-switch ($yn) { 
-"y"{RDiskCleanCmd} 
-"s"{RDiskCleanCmd} 
-}} #cleaning call if imput is Y or S
+switch ($yn) {
+"y"{RDiskCleanCmd}
+"s"{RDiskCleanCmd}
+}}
 else{}
-}
-#exeption handelling
-catch [System.Runtime.InteropServices.COMException] { Write-Host "Servidor RPC não disponivel" }  
-catch [System.Net.NetworkInformation.PingException] { Write-Host "Servidor RPC não disponivel" }     
-Catch [System.UnauthorizedAccessException] { Write-Host "Acesso Negado à recolha de informação do PC"}     
-Finally { try { Remove-Variable -ErrorAction Stop pcname , colItems, displayGB, objItem, iv4, osname, cpuname, serial, max, free, full, yn } 
+
+#Exeções
+} #Fim de Try
+catch [System.Runtime.InteropServices.COMException] { Write-Host "Servidor RPC não disponivel" }
+catch [System.Net.NetworkInformation.PingException] { Write-Host "Servidor RPC não disponivel" }
+Catch [System.UnauthorizedAccessException] { Write-Host "Acesso Negado à recolha de informação do PC"}   
+Finally { try { Remove-Variable -ErrorAction Stop pcname , colItems , osname , cpuname , serial , diskspace, recArray, yn} #Limpeza de variavéis (imperativo a limpesa de $pcname, pois o script verifica no inicio se a variavel é nula, não apagar a variavel fará o resto dos script executar com a introdução prévia)
 catch [System.Management.Automation.ItemNotFoundException] {} }
-}
-#in case the pc doesn't ping
-Else { Write-Host "Computador desligado ou não existente"  
-try { Remove-Variable -ErrorAction Stop pcname , contest}   
+} #Fim de If
+#Caso o pc não esteja ligado
+Else { Write-Host "Computador desligado ou não existente"
+try { Remove-Variable -ErrorAction Stop pcname , contest}
 catch [System.Management.Automation.ItemNotFoundException] {} }
-}
-main #call of scritpt
+#Fim
